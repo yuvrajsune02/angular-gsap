2ee0359c05603143affcd687c12d0f48a9b9bb67
Yuvraj Sune <yuvrajsune02@gmail.com>
2025-05-04 19:04:49 +0530

    added clip path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import gsap from 'gsap';
+import SplitText from 'gsap/SplitText';
 
+gsap.registerPlugin(SplitText);
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-gsap';
+  // @ViewChild('splitElement') splitElement!: ElementRef;
+  // private splitAnimation: gsap.core.Animation | null = null;
+
+  // ngAfterViewInit() {
+  //   // Ensure fonts are loaded
+
+  //   gsap.set(this.splitElement.nativeElement, { opacity: 1 });
+
+  //   const splitInstance = SplitText.create(this.splitElement.nativeElement, {
+  //     type: 'words,lines',
+  //     linesClass: 'line',
+  //     autoSplit: true,
+  //     mask: 'lines',
+  //     onSplit: (self: any) => {
+  //       this.splitAnimation = gsap.from(self.lines, {
+  //         duration: 0.9,
+  //         yPercent: 100,
+  //         opacity: 0,
+  //         stagger: 0.1,
+  //         ease: 'expo.out',
+  //       });
+  //     },
+  //   });
+  // }
+  // playAnimation() {
+  //   if (this.splitAnimation) {
+  //     this.splitAnimation.timeScale(0.2).play(0);
+  //   }
+  // }
 }
